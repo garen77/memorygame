e655356041e26aaf9eee40d792d1b924eddad622
garen77 <crescenzo.garofalo@gmail.com>
2020-08-05 17:16:32 +0200

    mobile part

---
+++ mobile/metro.config.js
@@ -0,0 +1,30 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+const path = require('path')
+
+const extraNodeModules = {
+  'shared': path.resolve(__dirname + '/../shared'),
+};
+const watchFolders = [
+  path.resolve(__dirname + '/../shared')
+];
+
+module.exports = {
+  
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  }, 
+  resolver: {
+    extraNodeModules
+  },
+  watchFolders
+};
